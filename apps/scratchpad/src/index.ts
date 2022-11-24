73b817eb53308c57bc78429afefedac3adf07b9a
Jack Wild <jack@isjackwild.com>
2022-11-24 17:56:47 +0000

    started to try a monorepo setup

---
+++ apps/scratchpad/src/index.ts
@@ -0,0 +1,81 @@
+import {
+  Renderer,
+  RenderProgram,
+  Geometry,
+  Uniforms,
+  Mesh,
+  TextureLoader,
+  TGeometryArgs,
+} from "@mini-gpu/core";
+
+import { primitives } from "twgl.js";
+import textureSrc from "./spectral-interference.png";
+import shader from "bundle-text:./shader.wgsl";
+
+let device: GPUDevice;
+const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+canvas.width = window.innerWidth * window.devicePixelRatio;
+canvas.height = window.innerHeight * window.devicePixelRatio;
+
+let renderer: Renderer;
+let uniforms;
+
+console.log(Mesh.foo);
+
+const requestWebGPU = async () => {
+  if (device) return device;
+
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    console.warn("Could not access Adapter");
+    return;
+  }
+  device = await adapter.requestDevice();
+  return device;
+};
+
+const render = () => {
+  uniforms.uniform.u_elapsed_time = uniforms.uniform.u_elapsed_time + 0.01;
+  renderer.render();
+  requestAnimationFrame(render);
+};
+
+const init = async () => {
+  if (!navigator.gpu) {
+    return alert(
+      "WebGPU not available! — Use Chrome Canary and enable-unsafe-gpu in flags."
+    );
+  }
+  const device = (await requestWebGPU()) as GPUDevice;
+  const texture = await new TextureLoader(device).loadTextureFromImageSrc(
+    textureSrc
+  );
+  renderer = new Renderer(device as GPUDevice, canvas);
+
+  const geometry = new Geometry(
+    renderer,
+    primitives.createSphereVertices(1, 64, 32) as TGeometryArgs
+  );
+  uniforms = new Uniforms(device as GPUDevice, {
+    u_elapsed_time: 0,
+    u_test_vector: [1, 1, 1],
+    u_test_vector_2: [1, 1, 1, 1],
+    u_test_vector_3: [1, 1],
+    u_delta_time: 0,
+    u_texture: texture,
+  });
+  const viewportUniforms = new Uniforms(device as GPUDevice, {
+    u_resolution: [canvas.width, canvas.height],
+  });
+  const program = new RenderProgram(renderer, shader, {
+    default: uniforms,
+    viewport: viewportUniforms,
+  });
+  // console.log(program.getWgslChunk());
+
+  const mesh = new Mesh(renderer, geometry, program);
+  renderer.addRenderable(mesh);
+  requestAnimationFrame(render);
+};
+
+init();
