0b4d012eb18c7ee4d8f49bf782dc2f42f3302d27
Jack Wild <jack@isjackwild.com>
2022-12-01 10:37:49 +0000

    request gpu not available message

--- src/core/Helpers.ts
+++ src/core/Helpers.ts
@@ -1,4 +1,10 @@
 const requestWebGPU: () => Promise<false | GPUDevice> = async () => {
+  if (!navigator.gpu) {
+    alert(
+      "WebGPU not available! — Use Chrome Canary and enable-unsafe-gpu in flags."
+    );
+    return false;
+  }
   const adapter = await navigator.gpu.requestAdapter();
   if (!adapter) {
     console.warn("Could not access Adapter");
