889e8f3bfcdd272c2a378cdb2e92aa3dd690c6a8
Jack Wild <jack@isjackwild.com>
2022-12-19 12:08:18 +0000

    not working

--- src/core/ComputeProgram.ts
+++ src/core/ComputeProgram.ts
@@ -5,13 +5,19 @@ import { ProgramInputInterface } from "./Program";
 // TODO — How to update a uniforms group and swap with another
 class ComputeProgram extends Program implements ComputableInterface {
   declare pipeline: GPUComputePipeline;
+  private workgroupTotalSize = 0;
+  private workgroupSize3D = [64, 1, 1];
 
   constructor(
     private device: GPUDevice,
     public shader: string,
     protected _inputs: { [key: string]: ProgramInputInterface },
     private count: number,
-    private workgroupSize: number
+    private workgroupSize:
+      | number
+      | [number]
+      | [number, number]
+      | [number, number, number]
   ) {
     super();
     this.inputsKeys = Object.keys(this.inputs);
@@ -30,7 +36,40 @@ class ComputeProgram extends Program implements ComputableInterface {
         entryPoint: "main",
       },
     });
+
+    if (Array.isArray(this.workgroupSize)) {
+      this.workgroupTotalSize =
+        (this.workgroupSize[0] || 1) *
+        (this.workgroupSize[1] || 1) *
+        (this.workgroupSize[2] || 1);
+
+      this.workgroupSize3D[0] = this.workgroupSize[0] || 1;
+      this.workgroupSize3D[1] = this.workgroupSize[1] || 1;
+      this.workgroupSize3D[2] = this.workgroupSize[2] || 1;
+    } else {
+      this.workgroupTotalSize = this.workgroupSize;
+    }
+
+    console.log(this.workgroupSize3D);
   }
+
+  // private calculateWorkgroupSize(): [number, number, number] {
+  //   let x = this.device.limits.maxComputeWorkgroupSizeX;
+  //   let y = this.device.limits.maxComputeWorkgroupSizeY;
+  //   let z = this.device.limits.maxComputeWorkgroupSizeZ;
+
+  //   while (x * y * z > this.count) {
+  //     x--;
+  //     y--;
+  //     z--;
+  //   }
+
+  //   return [
+  //     Math.max(Math.min(x + 1, this.device.limits.maxComputeWorkgroupSizeX), 1),
+  //     Math.max(Math.min(y + 1, this.device.limits.maxComputeWorkgroupSizeY), 1),
+  //     Math.max(Math.min(z + 1, this.device.limits.maxComputeWorkgroupSizeZ), 1),
+  //   ];
+  // }
 
   public setBindGroups(computePass: GPUComputePassEncoder): void {
     super.setBindGroups(computePass);
@@ -39,7 +78,13 @@ class ComputeProgram extends Program implements ComputableInterface {
   public getCommands(computePass: GPUComputePassEncoder): void {
     computePass.setPipeline(this.pipeline);
     this.setBindGroups(computePass);
-    computePass.dispatchWorkgroups(Math.ceil(this.count / this.workgroupSize));
+    const dispatchCount = Math.ceil(this.count / this.workgroupTotalSize);
+    // computePass.dispatchWorkgroups(Math.ceil(this.count / this.workgroupSize));
+    computePass.dispatchWorkgroups(
+      Math.ceil(this.count / this.workgroupSize3D[0]),
+      Math.ceil(this.count / this.workgroupSize3D[1]),
+      Math.ceil(this.count / this.workgroupSize3D[2])
+    );
   }
 }
 
